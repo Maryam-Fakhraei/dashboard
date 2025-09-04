dcf1a7f238f06187e145c954f29e716cf49fd515
Maryam-Fakhraei <M.fakhraeisari@gmail.com>
2025-09-04 16:55:53 +0330

    test login form

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -3,17 +3,9 @@
 const users = [
   {
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
-    name: 'admin',
-    role: "Admin",
-    email: 'admin@nextmail.com',
-    password: '123456',
-  },
-    {
-    id: '410544b2-4001-4020-9855-fec4b6a6442a',
-    name: 'user',
-    role: "User",
+    name: 'User',
     email: 'user@nextmail.com',
-    password: '98765432',
+    password: '123456',
   },
 ];
 
